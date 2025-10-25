4ef3d70179ff8dc0c7d4ed3f87e1e4f5ad8e25e8
The Keymaker - TGK Admin <the.keymaker@thegnostickey.com>
2025-10-25 10:34:04 +0100

    Fix Firebase storageBucket domain to appspot.com

--- src/js/page.js
+++ src/js/page.js
@@ -18,11 +18,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyDYrFIw9I3hManf1TqvP6FARZTC-MlMuz0",
   authDomain: "the-gnostic-key.firebaseapp.com",
   projectId: "the-gnostic-key",
-  storageBucket: "the-gnostic-key.firebasestorage.app",
+  storageBucket: "the-gnostic-key.appspot.com",
   messagingSenderId: "903609435224",
   appId: "1:903609435224:web:3031fc94c9fbbe78f8762d",
   measurementId: "G-KD96SXX3JY"
 };
+
 // === 🜂 Init ===
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
