ec5e664091fb1db5a3be770d28f5079405270a64
The Keymaker - TGK Admin <the.keymaker@thegnostickey.com>
2025-12-09 19:28:47 +0000

    Community layout Firebase IDs missing - added in

--- .11tydata.js
+++ .11tydata.js
@@ -1,12 +1,11 @@
 export default {
   eleventyComputed: {
     layout: (data) => {
-      // If a page explicitly sets a layout, do NOT override
-      if (data.layout) {
+      // If a layout is defined AND is not empty, keep it
+      if (typeof data.layout === "string" && data.layout.trim() !== "") {
         return data.layout;
       }
 
-      // Otherwise fall back to base
       return "base.njk";
     }
   }
