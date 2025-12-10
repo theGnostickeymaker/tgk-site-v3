cf271a53fc12d90487fa075dc64f565a77ef4f63
The Keymaker - TGK Admin <the.keymaker@thegnostickey.com>
2025-12-10 00:46:24 +0000

    Community Looking Fucking Awesome!

---
+++ src/js/community-tabs.js
@@ -0,0 +1,20 @@
+document.addEventListener("click", (event) => {
+  const el = event.target.closest(".page-tabs .tab-link");
+  if (!el) return;
+
+  const targetId = el.getAttribute("href");
+  if (!targetId || !targetId.startsWith("#")) return;
+
+  event.preventDefault();
+
+  const target = document.querySelector(targetId);
+  if (!target) return;
+
+  // Delay for JS-rendered content
+  setTimeout(() => {
+    target.scrollIntoView({
+      behaviour: "smooth",
+      block: "start"
+    });
+  }, 50);
+});
