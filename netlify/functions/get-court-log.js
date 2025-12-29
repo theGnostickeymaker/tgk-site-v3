f0255a2059bdcb49429cc981d9c8759b8af82bd5
The Keymaker - TGK Admin <the.keymaker@thegnostickey.com>
2025-12-29 09:55:26 +0000

    Court Log Setup started.

--- netlify/functions/get-court-log.js
+++ netlify/functions/get-court-log.js
@@ -3,7 +3,11 @@ const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN_KEY)),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
